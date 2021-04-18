cdf694ae614af505fc3ed294bbd3eec2474d1899
katsuya murano <murano@starryworks.co.jp>
2021-04-19 02:23:51 +0900

    マウス追従スクリプト作成

---
+++ 01_00_howto/Assets/Scripts/ControllerScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControllerScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private Vector3 position;
+    // スクリーン座標をワールド座標に変換した位置座標
+    private Vector3 screenToWorldPointPosition;
+
+    private Vector3 usePosition = new Vector3();
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Vector3でマウス位置座標を取得する
+        position = Input.mousePosition;
+        // Z軸修正
+        position.z = 10f;
+
+        // マウス位置座標をスクリーン座標からワールド座標に変換する
+        screenToWorldPointPosition = Camera.main.ScreenToWorldPoint(position);
+
+        Vector3 pos = (screenToWorldPointPosition - gameObject.transform.position) * 0.1f;
+        // ワールド座標に変換されたマウス座標を代入
+        gameObject.transform.position += pos;
+        gameObject.transform.Rotate(new Vector3(1, 1, 1));
+    }
+}
